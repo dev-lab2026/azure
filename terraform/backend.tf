278d8009e617ab7a0e211dff22b538fecb0f9d86
dev-lab2026 <tarek.ferjani@consort-group.com>
2026-08-26 14:34:47 +0200

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "reactcicd-prod-rg"
+    resource_group_name  = "terraform-state-rg"
     storage_account_name = "reactcicdtfstate"
     container_name       = "tfstate"
-    key                  = "interflow.tfstate"
+    key                  = "clarity.tfstate"
   }
 }
